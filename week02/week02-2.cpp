4ecc913f078dd5e798c065a45d6edc2fa135d889
14750243-lab <14750243@me.mcu.edu.tw>
2026-09-17 11:44:44 +0800

    week02

---
+++ week02/week02-2.cpp
@@ -0,0 +1,11 @@
+///week02-2.cpp 要使用命名空間
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "請問你叫什麼名子阿?";
+    string name; ///宣告字串 name
+    cin >> name;///上週教 cin 原來長這樣
+    cout << name << "你好, 今天教字串喔!";
+}
